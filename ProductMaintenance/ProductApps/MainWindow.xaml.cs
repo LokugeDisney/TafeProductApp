e7336282f204394621e1f459d536d95a63ddeee5
001192985 <Disney.LOKUGE@student.tafesa.edu.au>
2024-03-23 11:05:42 +1030

    Added the delivery charge $25.

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,10 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+
+                // Calculate total charge including delivery charge
+                decimal totalCharge = cProduct.TotalPayment + 25.00m;
+                totalChargeTextBox.Text = totalCharge.ToString();
             }
             catch (FormatException)
             {
